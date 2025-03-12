22b612e32c9cc71ab37ce2ca81e941609cc2064d
Okwudili Ezeme <okwudili.ezeme@ab-inbev.com>
2025-03-11 22:36:38 -0400

    updated the config class with correct variable names

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    AZURE_SEARCH_ENDPOINT: str = f"https://{os.getenv('AZURE_SEARCH_SERVICE_NAME')}.search.windows.net"
+    AZURE_SEARCH_KEY: str = os.getenv("AZURE_SEARCH_ADMIN_KEY")
+    AZURE_SEARCH_INDEX_NAME_PRODUCT: str = os.getenv("AZURE_SEARCH_INDEX_NAME_PRODUCT")  # Updated index name variable
+
+settings = Settings()
