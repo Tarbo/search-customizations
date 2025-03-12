df3e36492a2ba69477197f86324b9df024ae34b2
Okwudili Ezeme <okwudili.ezeme@ab-inbev.com>
2025-03-11 23:25:27 -0400

    updated the config file

--- app/config.py
+++ app/config.py
@@ -1,12 +1,18 @@
 import os
+import yaml
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings:
-    AZURE_SEARCH_ENDPOINT: str = f"https://{os.getenv('AZURE_SEARCH_SERVICE_NAME')}.search.windows.net"
-    AZURE_SEARCH_KEY: str = os.getenv("AZURE_SEARCH_ADMIN_KEY")
-    AZURE_SEARCH_INDEX_NAME_PRODUCT: str = os.getenv("AZURE_SEARCH_INDEX_NAME_PRODUCT")  # Updated index name variable
-    AZURE_SEARCH_QUERY_KEY: str = os.getenv("AZURE_SEARCH_QUERY_KEY")
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), '../config.yaml'), 'r') as file:
+            config = yaml.safe_load(file)
+        self.AZURE_SEARCH_SERVICE_NAME = os.getenv("AZURE_SEARCH_SERVICE_NAME")
+        self.AZURE_SEARCH_ADMIN_KEY = os.getenv("AZURE_SEARCH_ADMIN_KEY")
+        self.AZURE_SEARCH_QUERY_KEY = os.getenv("AZURE_SEARCH_QUERY_KEY")
+        self.AZURE_SEARCH_INDEX_NAME_PRODUCT = os.getenv("AZURE_SEARCH_INDEX_NAME_PRODUCT")
+        self.AZURE_SEARCH_ENDPOINT = f"https://{self.AZURE_SEARCH_SERVICE_NAME}.search.windows.net"
+        self.PRODUCT_INDEX_COLUMNS = config['product_index_columns']
 
 settings = Settings()
