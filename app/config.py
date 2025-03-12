f72a08f8637016f234ea399e61a248bd38e69cd5
Okwudili Ezeme <okwudili.ezeme@ab-inbev.com>
2025-03-11 23:03:17 -0400

    added query key

--- app/config.py
+++ app/config.py
@@ -7,5 +7,6 @@ class Settings:
     AZURE_SEARCH_ENDPOINT: str = f"https://{os.getenv('AZURE_SEARCH_SERVICE_NAME')}.search.windows.net"
     AZURE_SEARCH_KEY: str = os.getenv("AZURE_SEARCH_ADMIN_KEY")
     AZURE_SEARCH_INDEX_NAME_PRODUCT: str = os.getenv("AZURE_SEARCH_INDEX_NAME_PRODUCT")  # Updated index name variable
+    AZURE_SEARCH_QUERY_KEY: str = os.getenv("AZURE_SEARCH_QUERY_KEY")
 
 settings = Settings()
